1192d931869ea542337a90b1724eb4e0ef13699d
Samavia-11 <samaviarasool888@gmail.com>
2026-08-27 16:30:17 +0500

    Update next.config.ts

--- samavia-portfolio/next.config.ts
+++ samavia-portfolio/next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/samavia-portfolio",
+  assetPrefix: "/samavia-portfolio/",
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: __dirname,
   },
